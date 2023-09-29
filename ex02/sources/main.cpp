0915a9b2727c0e3755b1497344b356b5be38e549
CanIBeFii <filipe250502@gmail.com>
2023-09-29 17:21:27 +0100

    ex02: add initial files

---
+++ ex02/sources/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "Span.hpp"
+
+int	main( void ) {
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+
+	{
+		Span	span( 20000000 , 10 );
+
+		span.addNumber( 17 );
+		span.addNumber( 3000 );
+
+		std::cout << span.shortestSpan() << std::endl;
+		std::cout << span.longestSpan() << std::endl;
+	}
+
+	{
+		Span	span( 5 );
+
+		try {
+			span.shortestSpan();
+		} catch ( Span::NoSpanException& e ) {
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
+
+	{
+		Span	span( 5 );
+
+		try {
+			span.longestSpan();
+		} catch ( Span::NoSpanException& e ) {
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
+
+	{
+		Span	span( 5 );
+
+		try {
+			span.addNumber(6);
+			span.addNumber(3);
+			span.addNumber(17);
+			span.addNumber(9);
+			span.addNumber(11);
+			span.addNumber(6);
+			span.addNumber(3);
+			span.addNumber(17);
+			span.addNumber(9);
+			span.addNumber(11);
+		} catch ( Span::FullSpanException& e ) {
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
+
+	return ( 0 );
+}
