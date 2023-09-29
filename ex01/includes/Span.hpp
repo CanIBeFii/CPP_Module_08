466eaca6e4999044c9b46678a38905ab99a40aa2
CanIBeFii <filipe250502@gmail.com>
2023-09-29 16:18:12 +0100

    ex01: done

---
+++ ex01/includes/Span.hpp
@@ -0,0 +1,55 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <list>
+#include <cmath>
+
+class Span {
+	public:
+		// Constructors
+		Span( void );
+		Span( unsigned int n );
+		Span( unsigned int n, int number );
+		Span( const Span& copy );
+
+		// Destructor
+		~Span( void );
+
+		// Operators
+		Span&	operator=( const Span& copy );
+
+		// Methods
+		// Getters
+		unsigned int	getMaxSize( void ) const;
+
+		// Setters
+		void			setMaxSize( unsigned int n );
+
+		void			addNumber( int n );
+		int				shortestSpan( void );
+		int				longestSpan( void );
+
+		// Exceptions
+		class FullSpanException : public std::exception {
+			public:
+				virtual const char*	what( void ) const throw();
+		};
+
+		class NoSpanException : public std::exception {
+			public:
+				virtual const char*	what( void ) const throw();
+		};
+
+	private:
+		unsigned int	_maxSize;
+		std::list<int>	_list;
+};
+
+// Colors
+#define RESET   "\033[0m"
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+
+#endif
---
+++ ex01/sources/Span.cpp
@@ -0,0 +1,110 @@
+#include "Span.hpp"
+#include <iostream>
+
+Span::Span( void ) : _maxSize(0), _list( std::list<int>() ) {
+	std::cout << BOLDCYAN << "Span: " << RESET;
+	std::cout << BOLDYELLOW << "Default " << BOLDGREEN << "Constructor " << RESET;
+	std::cout << "called" << std::endl;
+}
+
+Span::Span( unsigned int n ) : _maxSize( n ), _list( std::list<int>() ) {
+	std::cout << BOLDCYAN << "Span: " << RESET;
+	std::cout << BOLDYELLOW << "Type " << BOLDGREEN << "Constructor " << RESET;
+	std::cout << "called" << std::endl;
+}
+
+Span::Span( unsigned int n, int number ) : _maxSize( n ), _list( std::list<int>( n / 2, number ) ) {
+	std::cout << BOLDCYAN << "Span: " << RESET;
+	std::cout << BOLDYELLOW << "Type " << BOLDGREEN << "Constructor " << RESET;
+	std::cout << "called" << std::endl;
+}
+
+Span::Span( const Span& copy ) : _maxSize( copy._maxSize ), _list( copy._list ) {
+	std::cout << BOLDCYAN << "Span: " << RESET;
+	std::cout << BOLDYELLOW << "Copy " << BOLDGREEN << "Constructor " << RESET;
+	std::cout << "called" << std::endl;
+}
+
+// Descructor
+Span::~Span( void ) {
+	std::cout << BOLDCYAN << "Span: " << RESET;
+	std::cout << BOLDRED << "Destructor " << RESET;
+	std::cout << "called" << std::endl;
+}
+
+// Operators
+Span&	Span::operator=( const Span& copy ) {
+	std::cout << BOLDCYAN << "Span: " << RESET;
+	std::cout << BOLDYELLOW << "Assignation " << RESET;
+	std::cout << "called" << std::endl;
+
+	if ( this != &copy ) {
+		this->_maxSize = copy._maxSize;
+		this->_list = copy._list;
+	}
+	return ( *this );
+}
+
+// Methods
+// Getters
+unsigned int	Span::getMaxSize( void ) const {
+	return ( _maxSize );
+}
+
+// Setters
+void			Span::setMaxSize( unsigned int n ) {
+	_maxSize = n;
+}
+
+void			Span::addNumber( int n ) {
+	if ( _list.size() == _maxSize ) {
+		throw FullSpanException();
+	}
+	_list.push_back( n );
+}
+
+int				Span::shortestSpan( void ) {
+	if ( _list.size() < 2 ) {
+		throw NoSpanException();
+	}
+
+	int	shortest = 2147483647;
+	std::list<int>::iterator iter = _list.begin();
+	std::list<int>::iterator previous = _list.begin();
+
+	int	difference;
+	for ( iter++ ; iter != _list.end(); iter++, previous++ ) {
+		difference = std::abs(*previous - *iter);
+		if ( difference < shortest ) {
+			shortest = difference;
+		}
+	}
+	return ( shortest );
+}
+
+int				Span::longestSpan( void ) {
+	if ( _list.size() < 2 ) {
+		throw NoSpanException();
+	}
+
+	int	longest = 0;
+	std::list<int>::iterator iter = _list.begin();
+	std::list<int>::iterator previous = _list.begin();
+
+	int	difference;
+	for ( iter++; iter != _list.end(); iter++, previous++ ) {
+		difference = std::abs( *previous - *iter );
+		if ( longest < difference ) {
+			longest = difference;
+		}
+	}
+	return ( longest );
+}
+
+const char*		Span::FullSpanException::what( void ) const throw() {
+	return ( "Span is already at max size!" );
+}
+
+const char*		Span::NoSpanException::what( void ) const throw() {
+	return ( "There's no numbers to have a span!" );
+}
